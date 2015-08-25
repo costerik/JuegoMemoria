4385eb726e0ea6c021e267c2092c918474308e6a
costerik <costerik@hotmail.com>
2015-08-24 20:02:07 -0500

    addded color and sounds

--- app/src/main/java/com/example/erik/juegomemoria/MainActivity.java
+++ app/src/main/java/com/example/erik/juegomemoria/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.erik.juegomemoria;
 
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,32 +13,85 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    private static final String [] BUTTONS_STATES={"button_1","button_2","button_3","button_4","button_5","button_6",
+                                            "button_7","button_8","button_9","button_10","button_11","button_12",
+                                            "button_13","button_14","button_15","button_16"};
+    private static final String BUTTON_CHECK_TEXT="btnChk1Txt";
+    private static final String BUTTON_CHECK_ID="btnID";
+    private static final String RANDOM_NUMBERS="randomNumbers";
+    private static final String TV_MESSAGE="tvMessage";
+    private static final String VALUE_I="valueI";
+    private static final String CHECK="check";
+    private static final String PAIRS="pairs";
     private Button btn [] = new Button[16];
     private TextView tv ;
-    private int pairs [];//{1,4,6,5,3,8,1,5,7,2,6,7,3,8,4,2};
+    private int pairs [];
     private int pairsII []= new int[16];
     private int check [] = new int[2];
     private Button checkBtn [] =new Button[2];
     private int i=0;
     private int numPairs=0;
+    private SoundPool sp;
+    int [] sounds=new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=(TextView)findViewById(R.id.textView);
-        for(int i=0;i<16;i++)
-            btn[i]=(Button)findViewById(R.id.button+i);
+        for(int i=0;i<16;i++) {
+            btn[i] = (Button) findViewById(R.id.button + i);
+            btn[i].setBackgroundColor(Color.rgb(randInt(0,240), randInt(0,240), randInt(0,240)));
+        }
 
         for(int i=0;i<16;i++)
             btn[i].setOnClickListener(this);
 
         pairs=pairsResult(pairsII);
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.LOLLIPOP){
+            AudioAttributes aa = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .build();
+
+
+            sp = new SoundPool.Builder().setMaxStreams(10)
+                    .setAudioAttributes(aa)
+                    .build();
+
+            sounds[0] = sp.load(this, R.raw.track_01, 1);
+            sounds[1] = sp.load(this, R.raw.track_02, 1);
+            sounds[2]=sp.load(this,R.raw.track_03,1);
+        }else{
+            sp=new SoundPool(10, AudioManager.STREAM_MUSIC,1);
+            sounds[0]=sp.load(this,R.raw.track_01,1);
+            sounds[1]=sp.load(this,R.raw.track_02,1);
+            sounds[2]=sp.load(this,R.raw.track_03,1);
+        }
+        //sp.play(sounds[2],1,1,1,-1,1);
+        if(savedInstanceState != null){
+            i=savedInstanceState.getInt(VALUE_I);
+            check[0]=savedInstanceState.getInt(CHECK);
+            numPairs=savedInstanceState.getInt(PAIRS);
+            for(int i=0;i<btn.length;i++) {
+                if(savedInstanceState.getInt(BUTTONS_STATES[i])==View.INVISIBLE){
+                    btn[i].setVisibility(View.INVISIBLE);
+                }else{
+                    btn[i].setVisibility(View.VISIBLE);
+                }
+                if(savedInstanceState.getInt(BUTTON_CHECK_ID)==btn[i].getId()){
+                    checkBtn[0]=btn[i];
+                    checkBtn[0].setText(savedInstanceState.getString(BUTTON_CHECK_TEXT));
+                }
+            }
+            pairs=savedInstanceState.getIntArray(RANDOM_NUMBERS);
+            tv.setText(savedInstanceState.getString(TV_MESSAGE));
+        }
     }
 
     @Override
@@ -57,12 +116,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState){
+        for(int i=0;i<btn.length;i++){
+            saveInstanceState.putInt(BUTTONS_STATES[i],btn[i].getVisibility());
+        }
+        if(checkBtn[0]!=null){
+            saveInstanceState.putString(BUTTON_CHECK_TEXT, checkBtn[0].getText().toString());
+            saveInstanceState.putInt(BUTTON_CHECK_ID, checkBtn[0].getId());
+        }
+        saveInstanceState.putIntArray(RANDOM_NUMBERS, pairs);
+        saveInstanceState.putString(TV_MESSAGE,tv.getText().toString());
+        saveInstanceState.putInt(VALUE_I,i);
+        saveInstanceState.putInt(CHECK,check[0]);
+        saveInstanceState.putInt(PAIRS,numPairs);
+        super.onSaveInstanceState(saveInstanceState);
+    }
     public static int randInt(int min, int max) {
         // Usually this can be a field rather than a method variable
         Random rand = new Random();
         // nextInt is normally exclusive of the top value,
         // so add 1 to make it inclusive
         int randomNum = rand.nextInt((max - min) + 1) + min;
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return randomNum;
     }
 
@@ -96,16 +177,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
             i++;
         }
-		/*System.out.println("------------------------------------");
-		for(int in=0;in<auxTemp.length;in++){
-			System.out.println("i: "+in+" => "+auxTemp[in]);
-		}
-		System.out.println("------------------------------------");*/
         int indice2=0;
         for(int indice=0;indice<auxTemp.length;indice++){
-            if(auxTemp[i]!=0){
-
-            }
             if(pair[indice]!=0){
                 OPair[indice]=pair[indice];
             }else if(pair[indice]==0){
@@ -115,11 +188,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         }
-
-		/*for(int in=0;in<pair.length;in++){
-			System.out.println("i: "+in+" => "+pair[in]);
-		}
-		System.out.println("------------------------------------");*/
         return OPair;
     }
 
@@ -134,7 +202,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if(i<2) {
+        sp.play(sounds[0],1,1,1,0,1);
             switch (view.getId()) {
                 case R.id.button:
                     btn[0].setText(String.valueOf(pairs[0]));
@@ -218,21 +286,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
             }
             i++;
-        }else{
             if(i==2) {
                 if (check[0] == check[1] && checkBtn[0].getId() != checkBtn[1].getId()) {
+                    sp.play(sounds[1],1,1,1,0,1);
                     numPairs++;
                     checkBtn[0].setVisibility(View.INVISIBLE);
                     checkBtn[1].setVisibility(View.INVISIBLE);
                     tv.setTextSize(40);
                     tv.setText("Pairs: "+numPairs);
-                } else {
-                    checkBtn[0].setText("");
-                    checkBtn[1].setText("");
-                }
+                } //else {
+                Handler h= new Handler();
+                h.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        checkBtn[0].setText("");
+                        checkBtn[1].setText("");
+                    }
+                }, 100);
+                //}
                 i=0;
             }
-        }
         if(gameOver(btn)){
             tv.setText("YOU WIN!!!");
         }
