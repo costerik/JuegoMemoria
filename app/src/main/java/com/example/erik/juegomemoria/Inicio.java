fa3e54a31b20920e202fd1c086438cf5e50c0476
costerik <costerik@hotmail.com>
2015-10-18 13:13:35 -0500

    Added presentation to the initial screen

--- app/src/main/java/com/example/erik/juegomemoria/Inicio.java
+++ app/src/main/java/com/example/erik/juegomemoria/Inicio.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
